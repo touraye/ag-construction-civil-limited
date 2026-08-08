fdf4290eef74a9aa86ccf7886eada5f066d5ed16
touraye <touraye07@gmail.com>
2026-08-08 15:49:36 +0000

    fit: update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,13 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {/* site header */}
-        <Header />
-        {children}
-        <Footer />
+      <body className="min-h-full flex flex-col">               
+        {children}        
       </body>
     </html>
   );
